a6d9047d437667d0b7b35288e56a90771c3b8823
Genadij1 <bgennadij265@icloud.com>
2023-03-18 00:02:29 +0200

    ex_2-done

--- PythonApplication/PythonApplication.py
+++ PythonApplication/PythonApplication.py
@@ -1,9 +1,3 @@
-num_1 = int(input('num_1 '))
-num_2 = int(input('num_2 '))
-num_3 = int(input('num_3 '))
-print (num_1+num_2+num_3)
-print (num_1*num_2*num_3)
-
 salary = int(input('value '))
 credit = int(input('credit '))
 utilities = int(input('utilities '))
